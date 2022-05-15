b8f27f87ce8ec62f4c933ccab6ed6db84c55df01
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-15 15:38:00 +0930

    Adjusted script for prompts to run through index.js, beginning with a createManager function. Also, testing createManager through console log

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
+//const fs = require("fs");
+//const Employee = require("./lib/Employee");
+//const Manager = require("./lib/Manager");
+//const Engineer = require("./lib/Engineer");
+//const Intern = require("./lib/Intern");
 
-// Initialise employee object
-const employeePrompts = new Employee();
+// Begin prompts with asking for manager details
+const createManager = () => {
+    return inquirer
+        .prompt([
+            {
+                type:"input",
+                name: "name",
+                message: "What is your name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your ID?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Enter your email address",
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is your office number?",
+            }
+        ])
+    .then((managerData) => {
+        const managerAnswers = {
+            name: managerData.name,
+            id: managerData.id,
+            email: managerData.email,
+            officeNumber: managerData.officeNumber,
+        }
 
-// Start the employee prompts
-employeePrompts.begin();
+        console.log(managerAnswers);
+    })
+}
+
+createManager();
