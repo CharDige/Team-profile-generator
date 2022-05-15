75def4aa1680f9c84e79522c9b72cde4baaab816
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-15 16:11:20 +0930

    Added createEmployees() function with prompts for each type of employee

--- index.js
+++ index.js
@@ -42,4 +42,58 @@ const createManager = () => {
     })
 }
 
-createManager();
+// After manager created, begin Employee prompts
+const createEmployees = () => {
+    return inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "role",
+                message: "Would you like to add an employee?",
+                choices: ["Engineer", "Intern", "No more employees to add"],
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is this employee's name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is this employee's ID?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Enter employee's email address",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Enter this Engineer's GitHub username",
+                when: (answers) => answers.role === "Engineer",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What school did this intern go to?",
+                when: (answers) => answers.role === "Intern",
+            }
+        ])
+    .then((employeesData) => {
+        const employeesAnswers = {
+            role: employeesData.role,
+            name: employeesData.name,
+            id: employeesData.id,
+            email: employeesData.email,
+            github: employeesData.github,
+            school: employeesData.school,
+        }
+
+        console.log(employeesData);
+    })
+}
+
+// createManager();
+
+createEmployees();
