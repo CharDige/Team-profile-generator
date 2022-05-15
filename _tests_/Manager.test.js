9e085ebbdda15112cce1a0a0867694e80039dbe8
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-15 22:45:10 +0930

    Added manager function tests

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,12 +1,29 @@
 const Employee = require("../lib/Employee");
 const Manager = require("../lib/Manager");
 
-// Constructor test
+// Manager constructor test
 describe("Constructor", () => {
     it("Can set an office number in the constructor", () => {
         const officeNumber = 123;
         const managerOfficeNumber = new Manager("Charlotte", 23, "chardige@email.com", officeNumber);
 
         expect(managerOfficeNumber.officeNumber).toBe(officeNumber);
+    })
+})
+
+// Manager function tests
+describe("Functions", () => {
+    it("Can get an office number via the getOfficerNumber() function", () => {
+        const officeNumber = 123;
+        const managerOfficeNumber = new Manager("Charlotte", 23, "chardige@email.com", officeNumber);
+
+        expect(managerOfficeNumber.getOfficeNumber()).toBe(officeNumber);
+    })
+
+    it("Can return 'Manager' via the getRole() function", () => {
+        const role = "Manager";
+        const newManager = new Manager("Charlotte", 23, "chardige@email.com", 123);
+
+        expect(newManager.getRole()).toBe(role);
     })
 })
