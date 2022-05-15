22c10b60aecb2724067f2df203b3e9963e4420cc
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-15 23:03:32 +0930

    Added intern function tests, and removed Employee module since it wasn't being used

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,12 +1,28 @@
-const Employee = require("../lib/Employee");
 const Intern = require("../lib/Intern");
 
 // Intern constructor test
 describe("Constructor", () => {
     it("Can set a school in the constructor", () => {
         const school = "University of Adelaide";
-        const internSchool = new Intern("Charlotte", 23, "chardige@email", school);
+        const internSchool = new Intern("Charlotte", 23, "chardige@email.com", school);
 
         expect(internSchool.school).toBe(school);
+    })
+})
+
+// Intern function tests
+describe("Functions", () => {
+    it("Can get a school via the getSchool() function", () => {
+        const school = "University of Adelaide";
+        const internSchool = new Intern("Charlotte", 23, "chardige@email.com", school);
+
+        expect(internSchool.getSchool()).toBe(school);
+    })
+
+    it("Can return 'Intern' via the getRole() function", () => {
+        const role = "Intern";
+        const newIntern = new Intern("Charlotte", 23, "chardige@email.com", "University of Adelaide");
+
+        expect(newIntern.getRole()).toBe(role);
     })
 })
