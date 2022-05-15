c2d55c1e09eb9b7275f1b6bf0d9a88bf53ee7069
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-15 22:35:04 +0930

    Added Employee function tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require("../lib/Employee");
 
-// Constructor tests
+// Employee constructor tests
 describe("Constructor", () => {
     it("Can create a new Employee object", () => {
         const newEmployee = new Employee();
@@ -26,5 +26,36 @@ describe("Constructor", () => {
         const employeeEmail = new Employee("Charlotte", 23, email);
 
         expect(employeeEmail.email).toBe(email);
+    })
+})
+
+// Employee function tests
+describe("Functions", () => {
+    it("Can get a name via the getName() function", () => {
+        const name = "Charlotte";
+        const employeeName = new Employee(name);
+
+        expect(employeeName.getName()).toBe(name);
+    })
+
+    it("Can get an id via the getId() function", () => {
+        const id = 23;
+        const employeeId = new Employee("Charlotte", id);
+
+        expect(employeeId.getId()).toBe(id);
+    })
+
+    it("Can get an email via the getEmail() function", () => {
+        const email = "chardige@email.com";
+        const employeeEmail = new Employee("Charlotte", 23, email);
+
+        expect(employeeEmail.getEmail()).toBe(email);
+    })
+
+    it("Can get the role to be 'Employee' via the getRole() function", () => {
+        const role = "Employee";
+        const newEmployee = new Employee("Charlotte", 23, "chardige@email.com");
+
+        expect(newEmployee.getRole()).toBe(role);
     })
 })
