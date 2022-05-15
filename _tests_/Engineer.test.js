50a5234e4a1daf6d8c34937ab64ea17eb0665d3f
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-15 22:53:39 +0930

    Added engineer function tests

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -10,3 +10,20 @@ describe("Constructor", () => {
         expect(engineerGitHub.github).toBe(github);
     })
 })
+
+// Engineer function tests
+describe("Functions", () => {
+    it("Can get a GitHub username via the getGitHub() function", () => {
+        const github = "CharDige";
+        const engineerGitHub = new Engineer("Charlotte", 23, "chardige@email.com", github);
+
+        expect(engineerGitHub.github).toBe(github);
+    })
+
+    it("Can return 'Engineer' via the getRole() function", () => {
+        const role = "Engineer";
+        const newEngineer = new Engineer("Charlotte", 23, "chardige@email.com", "CharDige");
+
+        expect(newEngineer.getRole()).toBe(role);
+    })
+})
